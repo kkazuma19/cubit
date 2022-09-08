cc9edcdab05ff9e3fb7f591352558bccc387de3a
Kazuma Kobayashi <kk55f@rtslm4vr9qhhjdevicemstedu.local>
2022-09-07 23:34:19 -0500

    basic fuel pellet script

--- pellet.py
+++ pellet.py
@@ -3,32 +3,32 @@ import numpy as np
 import cubit
 
 # define the parametric values
-# fuel pellet
+## fuel pellet
 f_radius = 4.1
 f_height = 2 * 5.93
 
+## dish
+dish_sph_radius = 10.1542
+dish_depth = 0.3
+
+## chamfer
+chamf_radius = 0.25
 
 command = "create cylinder height " + str(f_height) + " radius " + str(f_radius)
 cubit.cmd(command)
+command = "modify curve 1 2 chamfer radius " + str(chamf_radius)
+cubit.cmd(command)
 
 # vol id 2
-command = "create sphere radius 10.1542" 
+command = "create sphere radius " + str(dish_sph_radius) 
 cubit.cmd(command)
 
-command = "move Volume 2 x 0 y 0 z 15.6842 include_merged"
+command = "move Volume 2 x 0 y 0 z " + str( f_height/2 + (dish_sph_radius - dish_depth) ) + " include_merged"
 cubit.cmd(command)
 
+# vol id 3
 command = "Volume 2 copy reflect z nomesh"
 cubit.cmd(command)
 
 command = "subtract volume 2 3 from volume 1"
 cubit.cmd(command)
-
-# inner cylinder
-#command = "create cylinder height " + str(c_height - 2*c_thick) + " radius " + str(c_in_radius)
-#cubit.cmd(command)
-# subtract 
-#command = "subtract volume 2 from volume 1"
-#cubit.cmd(command)
-
-
